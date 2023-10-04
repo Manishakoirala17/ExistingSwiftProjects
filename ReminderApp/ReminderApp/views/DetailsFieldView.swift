0a44557fb60f817bfccdb75fccd713b93cc0c90a
Manisha <manisha@dreamtusk.com>
2023-10-04 14:30:34 +0530

    m

---
+++ ReminderApp/ReminderApp/views/DetailsFieldView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailsFieldView.swift
+//  ReminderApp
+//
+//  Created by Manisha on 04/10/23.
+//
+
+import SwiftUI
+
+struct DetailsFieldView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailsFieldView()
+}
