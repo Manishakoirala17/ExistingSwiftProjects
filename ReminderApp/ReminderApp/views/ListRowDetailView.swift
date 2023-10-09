5c6d2e86426d6cebb042cd398cba6fd4e269234b
Manisha <manisha@dreamtusk.com>
2023-10-09 21:18:03 +0530

    New reminder row added action

--- ReminderApp/ReminderApp/views/ListRowDetailView.swift
+++ ReminderApp/ReminderApp/views/ListRowDetailView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ListRowDetailView: View {
     @State var isDetailPresented:Bool = false
+    @State var rowCount:Int = 0
     var color: Color = .orange
     var body: some View {
         VStack(alignment: .leading){
@@ -17,44 +18,72 @@ struct ListRowDetailView: View {
                 .bold()
                 .foregroundColor(color)
             List{
-                VStack(alignment:.leading){
-                    HStack{
-                        HStack(spacing:0){
-                            Image(systemName: "exclamationmark")
-                            Image(systemName: "exclamationmark")
-                            Image(systemName: "exclamationmark")
-                        }
-                        .foregroundColor(color)
-                        TextField("",text: .constant("Test Reminder 1"),axis: .vertical)
-                        Spacer()
-                            Image(systemName: "info.circle")
-                                .foregroundColor(color)
-                                .onTapGesture {
-                                    isDetailPresented = true
+                ForEach(0...rowCount,id:\.self){ _ in
+                    reminderRow(isDetailPresented: $isDetailPresented, color: color)
+                        .swipeActions{
+                            Button(role: .destructive, action: {}){
+                                Text("Delete")
+                            }
+                            }
+                            .swipeActions{
+                                Button(action:{
+                                    
+                                }){
+                                    Text("Detail")
                                 }
-                        
-                      
-                    }
-                    TextField("Add Note",text: .constant(""),axis: .vertical)
-                    Text("Today 9.00 pm")
-                        .foregroundColor(.red)
+                            }
                 }
             }
             .listStyle(.plain)
+            
             Spacer()
-            HStack{
-                Image(systemName: "plus")
-                Text("New Reminder")
-                    .bold()
+            Button(action:{
+                rowCount = rowCount + 1
+            }){
+                HStack{
+                    Image(systemName: "plus")
+                    Text("New Reminder")
+                        .bold()
+                }
+                .foregroundColor(color)
+                .padding()
             }
-            .foregroundColor(color)
-            .padding()
+           
           
         }
       
     }
 }
+struct reminderRow:View {
+    @Binding var isDetailPresented:Bool
+    var color: Color
+    
+    var body: some View {
+        VStack(alignment:.leading){
+            HStack{
+                HStack(spacing:0){
+                    Image(systemName: "exclamationmark")
+                    Image(systemName: "exclamationmark")
+                    Image(systemName: "exclamationmark")
+                }
+                .foregroundColor(color)
+                TextField("",text: .constant("Test Reminder 1"),axis: .vertical)
+                Spacer()
+                    Image(systemName: "info.circle")
+                        .foregroundColor(color)
+                        .onTapGesture {
+                            isDetailPresented = true
+                        }
+                
+              
+            }
+            TextField("Add Note",text: .constant(""),axis: .vertical)
+            Text("Today 9.00 pm")
+                .foregroundColor(.red)
+        }
 
+    }
+}
 #Preview {
     ListRowDetailView()
 }
